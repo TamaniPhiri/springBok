035b6b62e4ec15d7647dc3f3187ea9b1d3365b0e
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 05:47:29 -1000

    dto

--- src/main/java/com/booted/springy/mapper/UserMapper.java
+++ src/main/java/com/booted/springy/mapper/UserMapper.java
@@ -5,12 +5,18 @@ import com.booted.springy.entity.User;
 
 public class UserMapper {
     public static UserDto mapToUserDto(User user){
-        return  new UserDto(
+        return new UserDto(
                 user.getId(),
                 user.getName(),
                 user.getEmail()
         );
     }
 
-    public static User mapToUser
+    public static User mapToUser(UserDto userDto){
+        return new User(
+                userDto.getId(),
+                userDto.getEmail(),
+                userDto.getName()
+        );
+    }
 }
