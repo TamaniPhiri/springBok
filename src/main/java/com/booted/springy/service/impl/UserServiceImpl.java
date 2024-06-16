f037a3e7e0410d26f2db2f0918740262604c9dce
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 05:52:54 -1000

    dto

--- src/main/java/com/booted/springy/service/impl/UserServiceImpl.java
+++ src/main/java/com/booted/springy/service/impl/UserServiceImpl.java
@@ -2,11 +2,16 @@ package com.booted.springy.service.impl;
 
 import com.booted.springy.dto.UserDto;
 import com.booted.springy.service.UserService;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class UserServiceImpl implements UserService {
+
 
     @Override
     public UserDto createUser(UserDto userDto) {
+
         return null;
     }
 }
