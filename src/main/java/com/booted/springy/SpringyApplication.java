ee04d0080c659d19ea6680cc3c9d15a6a3fc7897
tamani phiri <tamanigabriel0@gmail.com>
2024-06-15 20:40:51 -1000

    spring init

---
+++ src/main/java/com/booted/springy/SpringyApplication.java
@@ -0,0 +1,13 @@
+package com.booted.springy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booted/springy/SpringyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booted.springy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
