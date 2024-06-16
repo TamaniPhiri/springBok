e172b16e0fb776dc8fb10598641dd14c2a9a90e7
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 05:57:56 -1000

    dto

--- src/main/java/com/booted/springy/service/impl/UserServiceImpl.java
+++ src/main/java/com/booted/springy/service/impl/UserServiceImpl.java
@@ -1,17 +1,17 @@
 package com.booted.springy.service.impl;
-
 import com.booted.springy.dto.UserDto;
+import com.booted.springy.entity.User;
+import com.booted.springy.mapper.UserMapper;
 import com.booted.springy.service.UserService;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class UserServiceImpl implements UserService {
-
-
+    private UserService userService;
     @Override
     public UserDto createUser(UserDto userDto) {
-
+        User user= UserMapper.mapToUser(userDto);
         return null;
     }
 }
