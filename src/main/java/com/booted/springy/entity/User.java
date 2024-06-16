ca99c52149667cba09d9cd246a0c44ed10e8d070
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 01:54:17 -1000

    psql added

--- src/main/java/com/booted/springy/entity/User.java
+++ src/main/java/com/booted/springy/entity/User.java
@@ -1,4 +1,19 @@
 package com.booted.springy.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "User")
 public class User {
+    private Long id;
+    private String email;
+    private String name;
 }
