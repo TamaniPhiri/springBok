04b1711fdea73a589d18a0b56bb2b121f42e4ce4
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 06:39:32 -1000

    mail service

--- src/main/java/com/booted/springy/controller/UserController.java
+++ src/main/java/com/booted/springy/controller/UserController.java
@@ -2,6 +2,7 @@ package com.booted.springy.controller;
 
 import com.booted.springy.dto.UserDto;
 import com.booted.springy.entity.User;
+import com.booted.springy.service.MailService;
 import com.booted.springy.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
     private UserService userService;
+    private MailService mailService;
 
     @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createdUser=userService.createUser(userDto);
+        mailService.sendMail(createdUser.getEmail(),"Account Creation","Thanks and welcome");
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 }
