0167017ae4095f6f68d00054d4698065e9e5abc7
tamani phiri <tamanigabriel0@gmail.com>
2024-06-16 22:49:15 -1000

    delete user by id

--- src/main/java/com/booted/springy/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/booted/springy/exceptions/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package com.booted.springy.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException(String message){
         super(message);
